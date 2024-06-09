d8f47e773e0d4a945b72e12ee1e593117df32020
Tim Herndon <39970215+Dragennd@users.noreply.github.com>
2024-06-09 10:40:16 -0500

    Removed Dependency Injection configuration since it will not be used in this app so as to reduce overhead

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,33 +6,6 @@ namespace NetworkAnalyzer
 {
     public partial class App : Application
     {
-        public static IHost? AppHost { get; private set; }
-
-        public App()
-        {
-            AppHost = Host.CreateDefaultBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddSingleton<MainWindow>();
-                })
-                .Build();
-        }
-
-        protected override async void OnStartup(StartupEventArgs e)
-        {
-            await AppHost!.StartAsync();
-
-            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
-            startupForm.Show();
-
-            base.OnStartup(e);
-        }
-
-        protected override async void OnExit(ExitEventArgs e)
-        {
-            await AppHost!.StopAsync();
-
-            base.OnExit(e);
-        }
+        
     }
 }
