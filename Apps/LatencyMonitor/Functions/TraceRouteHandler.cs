650e42cfe9980a151f939a4176c5d5b3d021d624
Tim Herndon <39970215+Dragennd@users.noreply.github.com>
2024-08-04 14:43:52 -0500

    Fixed issue where the Network Analyzer would crash if the traceroute failed to resolve the target at all

--- Apps/LatencyMonitor/Functions/TraceRouteHandler.cs
+++ Apps/LatencyMonitor/Functions/TraceRouteHandler.cs
@@ -19,86 +19,95 @@ namespace NetworkAnalyzer.Apps.LatencyMonitor.Functions
             PacketLossHandler packetLossHandler = new();
             TimeStampHandler timeStampHandler = new();
 
-            int hop = 1;
-            IPHostEntry targetHostEntry = await Dns.GetHostEntryAsync(targetName);
-            string targetIP = targetHostEntry.AddressList.First(a => a.AddressFamily == AddressFamily.InterNetwork).ToString();
-            LatencyMonitorSessionStatus lmsStatus;
             TracerouteStatus trStatus = TracerouteStatus.Failed;
 
-            while (hop <= totalHops)
+            try
             {
-                var response = await GetNextHopDestinationAsync(targetIP, hop);
+                int hop = 1;
+                IPHostEntry targetHostEntry = await Dns.GetHostEntryAsync(targetName);
+                string targetIP = targetHostEntry.AddressList.First(a => a.AddressFamily == AddressFamily.InterNetwork).ToString();
+                LatencyMonitorSessionStatus lmsStatus;
 
-                // Check for favorable status and that hop be greater than 0 but less than 3
-                // If so, set status to Up
-                if ((response.status == IPStatus.Success || response.status == IPStatus.TtlExpired) && hop < 3)
+                while (hop <= totalHops && trStatus != TracerouteStatus.Unresolved)
                 {
-                    lmsStatus = LatencyMonitorSessionStatus.Up;
+                    var response = await GetNextHopDestinationAsync(targetIP, hop);
+
+                    // Check for favorable status and that hop be greater than 0 but less than 3
+                    // If so, set status to Up
+                    if ((response.status == IPStatus.Success || response.status == IPStatus.TtlExpired) && hop < 3)
+                    {
+                        lmsStatus = LatencyMonitorSessionStatus.Up;
+                    }
+                    // Check for favorable status and that hop be greater than or equal to 3 and that latency be greater than 0
+                    // If so, set status to Up
+                    else if ((response.status == IPStatus.Success || response.status == IPStatus.TtlExpired) && hop >= 3 && response.latency > 0)
+                    {
+                        lmsStatus = LatencyMonitorSessionStatus.Up;
+                    }
+                    else
+                    {
+                        lmsStatus = LatencyMonitorSessionStatus.Down;
+                    }
+
+                    if (response.ipAddress.ToString() == targetIP)
+                    {
+                        // Create a new dictionary entry for the final hop in the Traceroute
+                        await manager.CreateSessionAsync(response.ipAddress,
+                            await manager.NewSessionDataAsync(response.ipAddress, response.latency, lmsStatus,
+                                await latencyHandler.CalculateLowestLatencyAsync(response.status, response.latency, response.ipAddress, true),
+                                await latencyHandler.CalculateHighestLatencyAsync(response.status, response.latency, response.ipAddress, true),
+                                await latencyHandler.CalculateAverageLatencyAsync(response.status, response.latency, response.ipAddress, true),
+                                await latencyHandler.CalculateTotalLatencyAsync(response.latency, response.ipAddress, true),
+                                await packetLossHandler.CalculateTotalPacketsLostAsync(response.status, response.ipAddress, true),
+                                await packetLossHandler.CalculateFailedPingAsync(response.status, response.ipAddress, true),
+                                await timeStampHandler.CalculateTimeStampAsync(),
+                                hop));
+
+                        // Add the final hop target to the IPAddresses list to continue the scan
+                        IPAddresses.Add(response.ipAddress);
+
+                        trStatus = TracerouteStatus.Completed;
+                        break;
+                    }
+                    else if (response.ipAddress.ToString() != targetIP && response.ipAddress.ToString() != "0.0.0.0")
+                    {
+                        // Create a new dictionary entry for the specified hop in the Traceroute
+                        await manager.CreateSessionAsync(response.ipAddress,
+                            await manager.NewSessionDataAsync(response.ipAddress, response.latency, lmsStatus,
+                                await latencyHandler.CalculateLowestLatencyAsync(response.status, response.latency, response.ipAddress, true),
+                                await latencyHandler.CalculateHighestLatencyAsync(response.status, response.latency, response.ipAddress, true),
+                                await latencyHandler.CalculateAverageLatencyAsync(response.status, response.latency, response.ipAddress, true),
+                                await latencyHandler.CalculateTotalLatencyAsync(response.latency, response.ipAddress, true),
+                                await packetLossHandler.CalculateTotalPacketsLostAsync(response.status, response.ipAddress, true),
+                                await packetLossHandler.CalculateFailedPingAsync(response.status, response.ipAddress, true),
+                                await timeStampHandler.CalculateTimeStampAsync(),
+                                hop));
+
+                        // Add the specified hop target to the IPAddresses list to continue the scan
+                        IPAddresses.Add(response.ipAddress);
+
+                        trStatus = TracerouteStatus.Failed;
+                    }
+                    else
+                    {
+                        await manager.CreateSessionAsync(await GenerateNoResponseKeyAsync(),
+                            await manager.NewSessionDataAsync("Request timed out", hop));
+
+                        trStatus = TracerouteStatus.Failed;
+                    }
+
+                    hop++;
                 }
-                // Check for favorable status and that hop be greater than or equal to 3 and that latency be greater than 0
-                // If so, set status to Up
-                else if ((response.status == IPStatus.Success || response.status == IPStatus.TtlExpired) && hop >= 3 && response.latency > 0)
-                {
-                    lmsStatus = LatencyMonitorSessionStatus.Up;
-                }
-                else
-                {
-                    lmsStatus = LatencyMonitorSessionStatus.Down;
-                }
-
-                if (response.ipAddress.ToString() == targetIP)
-                {
-                    // Create a new dictionary entry for the final hop in the Traceroute
-                    await manager.CreateSessionAsync(response.ipAddress,
-                        await manager.NewSessionDataAsync(response.ipAddress, response.latency, lmsStatus,
-                            await latencyHandler.CalculateLowestLatencyAsync(response.status, response.latency, response.ipAddress, true),
-                            await latencyHandler.CalculateHighestLatencyAsync(response.status, response.latency, response.ipAddress, true),
-                            await latencyHandler.CalculateAverageLatencyAsync(response.status, response.latency, response.ipAddress, true),
-                            await latencyHandler.CalculateTotalLatencyAsync(response.latency, response.ipAddress, true),
-                            await packetLossHandler.CalculateTotalPacketsLostAsync(response.status, response.ipAddress, true),
-                            await packetLossHandler.CalculateFailedPingAsync(response.status, response.ipAddress, true),
-                            await timeStampHandler.CalculateTimeStampAsync(),
-                            hop));
-
-                    // Add the final hop target to the IPAddresses list to continue the scan
-                    IPAddresses.Add(response.ipAddress);
-
-                    trStatus = TracerouteStatus.Completed;
-                    break;
-                }
-                else if (response.ipAddress.ToString() != targetIP && response.ipAddress.ToString() != "0.0.0.0")
-                {
-                    // Create a new dictionary entry for the specified hop in the Traceroute
-                    await manager.CreateSessionAsync(response.ipAddress,
-                        await manager.NewSessionDataAsync(response.ipAddress, response.latency, lmsStatus,
-                            await latencyHandler.CalculateLowestLatencyAsync(response.status, response.latency, response.ipAddress, true),
-                            await latencyHandler.CalculateHighestLatencyAsync(response.status, response.latency, response.ipAddress, true),
-                            await latencyHandler.CalculateAverageLatencyAsync(response.status, response.latency, response.ipAddress, true),
-                            await latencyHandler.CalculateTotalLatencyAsync(response.latency, response.ipAddress, true),
-                            await packetLossHandler.CalculateTotalPacketsLostAsync(response.status, response.ipAddress, true),
-                            await packetLossHandler.CalculateFailedPingAsync(response.status, response.ipAddress, true),
-                            await timeStampHandler.CalculateTimeStampAsync(),
-                            hop));
-
-                    // Add the specified hop target to the IPAddresses list to continue the scan
-                    IPAddresses.Add(response.ipAddress);
-
-                    trStatus = TracerouteStatus.Failed;
-                }
-                else
-                {
-                    await manager.CreateSessionAsync(await GenerateNoResponseKeyAsync(),
-                        await manager.NewSessionDataAsync("Request timed out", hop));
-
-                    trStatus = TracerouteStatus.Failed;
-                }
-
-                hop++;
+            }
+            catch (SocketException)
+            {
+                trStatus = TracerouteStatus.Unresolved;
             }
 
             return trStatus;
         }
 
+        // Copy the number which was assigned to the target from round to round so it maintains its order
         public async Task<int> MaintainAssignedHopAsync(string targetName)
         {
             LatencyMonitorData lastDataSet = LiveSessionData[targetName].LastOrDefault();
@@ -117,6 +126,7 @@ namespace NetworkAnalyzer.Apps.LatencyMonitor.Functions
             return await Task.FromResult(hop);
         }
 
+        // Create a unique key to use if the target found for a specified hop fails to send a ping reply
         private async Task<string> GenerateNoResponseKeyAsync()
         {
             var keys = LiveSessionData.Keys;
@@ -135,6 +145,7 @@ namespace NetworkAnalyzer.Apps.LatencyMonitor.Functions
             return await Task.FromResult(responseKey);
         }
 
+        // Ping the target with an ever-increasing TTL and return the data from that hop to the Traceroute
         private async Task<(string ipAddress, IPStatus status, int latency)> GetNextHopDestinationAsync(string targetName, int hop)
         {
             var target = Dns.GetHostAddresses(targetName).First(address => address.AddressFamily == AddressFamily.InterNetwork);
--- Apps/Models/TracerouteStatus.cs
+++ Apps/Models/TracerouteStatus.cs
@@ -3,6 +3,7 @@
     internal enum TracerouteStatus
     {
         Completed = 1,
-        Failed = 2
+        Failed = 2,
+        Unresolved = 3 // Only use this if the traceroute fails completely
     }
 }
