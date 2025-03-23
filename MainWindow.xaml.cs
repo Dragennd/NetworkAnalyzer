fe3a7cce30e3d72a434b558cab3e3dbbdf7efdb6
Tim Herndon <dragennd@gmail.com>
2025-03-23 17:14:14 -0500

    Relocated the GenerateDatabase method from the MainWindowViewModel to the MainWindow Code Behind Removed the MainWindowViewModel since all it was doing was handling the View and not handling anything Model related

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,8 +3,11 @@ using NetworkAnalyzer.Apps.IPScanner;
 using NetworkAnalyzer.Apps.LatencyMonitor;
 using NetworkAnalyzer.Apps.Reports;
 using NetworkAnalyzer.Apps.Settings;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
+using static NetworkAnalyzer.Apps.GlobalClasses.DataStore;
 
 namespace NetworkAnalyzer
 {
@@ -26,6 +29,7 @@ namespace NetworkAnalyzer
             MainContentControl.Content = HomeControl;
             BtnHome.IsChecked = true;
             HomeConnectionUtility.SendUpdateChangelogRequest();
+            GenerateDatabase();
         }
 
         private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
@@ -74,6 +78,20 @@ namespace NetworkAnalyzer
         private void SettingsButton_Checked(object sender, RoutedEventArgs e)
         {
             MainContentControl.Content = SettingsControl;
+        }
+
+        private void GenerateDatabase()
+        {
+            if (!File.Exists(DatabasePath))
+            {
+                using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(LocalDatabasePath))
+                {
+                    using (FileStream fileStream = new FileStream(DatabasePath, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+            }
         }
     }
 }
