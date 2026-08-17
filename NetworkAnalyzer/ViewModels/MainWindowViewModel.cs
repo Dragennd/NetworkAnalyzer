96ef8153fa58c37f12dedcc986f2f898f2ca31e3
Dragennd <dragennd@gmail.com>
2026-08-16 19:43:54 -0500

    Implemented notification toasts

--- NetworkAnalyzer/ViewModels/MainWindowViewModel.cs
+++ NetworkAnalyzer/ViewModels/MainWindowViewModel.cs
@@ -26,9 +26,13 @@ namespace NetworkAnalyzer.ViewModels;
 internal partial class MainWindowViewModel : ObservableValidator
 {
     public ObservableCollection<NotificationInfo> Notifications { get; set; } = new();
+    public ObservableCollection<NotificationInfo> NotificationsQueue { get; set; } = new();
     
     [ObservableProperty]
     public partial UserControl Content { get; set; }
+    
+    [ObservableProperty]
+    public partial NotificationInfo CurrentNotification { get; set; }
     
     [ObservableProperty]
     public partial string ContentTitle { get; private set; }
@@ -83,13 +87,21 @@ internal partial class MainWindowViewModel : ObservableValidator
     } = false;
 
     [ObservableProperty]
+    public partial bool IsNotificationToastVisible { get; set; } = false;
+    
+    private bool IsNotificationProcessorActive { get; set; } = false;
+
+    [ObservableProperty]
     public partial bool IsPkexecUnavailable { get; set; } = true;
 
     [ObservableProperty]
     public partial bool IsRedDotBadgeVisible { get; set; } = false;
-    
+
     [ObservableProperty]
     public partial string CodeToCopy { get; private set; }
+
+    [ObservableProperty]
+    public partial double NotificationProgress { get; set; }
 
     [ObservableProperty]
     public partial MaterialIconKind OptionsIcon { get; set; } = MaterialIconKind.MenuRightOutline;
@@ -121,12 +133,17 @@ internal partial class MainWindowViewModel : ObservableValidator
         _mainController = mainController;
 
         Notifications.CollectionChanged += Notifications_CollectionChanged;
+        NotificationsQueue.CollectionChanged += NotificationsQueue_CollectionChanged;
         _mainController.AddNotifications += AddNewNotification;
         _mainController.RemoveNotifications += RemoveNotification;
         
+        // To-Do: Remove dummy test info prior to final build
         Notifications.Add(new NotificationInfo("Test Warning", "This is a very long sentence, designed to allow me to measure how the text wraps on the screen. Hopefully this is long enough that I can see how it displays and make changes as needed.", MaterialIconKind.AlertOutline));
         Notifications.Add(new NotificationInfo("Test Error", "This is a test,\n only a test\n Bwhuahaha", MaterialIconKind.AlertCircleOutline));
         Notifications.Add(new NotificationInfo("Test", "This is a test,\n only a test\n Bwhuahaha", MaterialIconKind.InformationOutline));
+        NotificationsQueue.Add(new NotificationInfo("Test Warning", "This is a very long sentence, designed to allow me to measure how the text wraps on the screen. Hopefully this is long enough that I can see how it displays and make changes as needed.", MaterialIconKind.AlertOutline));
+        NotificationsQueue.Add(new NotificationInfo("Test Error", "This is a test,\n only a test\n Bwhuahaha", MaterialIconKind.AlertCircleOutline));
+        NotificationsQueue.Add(new NotificationInfo("Test", "This is a test,\n only a test\n Bwhuahaha", MaterialIconKind.InformationOutline));
 
         Content = _home;
         ContentTitle = "Home";
@@ -265,6 +282,7 @@ internal partial class MainWindowViewModel : ObservableValidator
     private void AddNewNotification(NotificationInfo notification)
     {
         Notifications.Add(notification);
+        NotificationsQueue.Add(notification);
     }
 
     private void Notifications_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -272,6 +290,57 @@ internal partial class MainWindowViewModel : ObservableValidator
         if (Notifications.Count > 0 && !IsNotificationCenterCardVisible)
         {
             IsRedDotBadgeVisible = true;
+        }
+    }
+
+    private void NotificationsQueue_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (IsNotificationProcessorActive)
+            return;
+
+        _ = ProcessIncomingNotificationsAsync();
+    }
+
+    private async Task ProcessIncomingNotificationsAsync()
+    {
+        if (IsNotificationProcessorActive)
+            return;
+        
+        IsNotificationProcessorActive = true;
+
+        try
+        {
+            while (NotificationsQueue.Count > 0)
+            {
+                IsNotificationToastVisible = true;
+                var notification = NotificationsQueue[0];
+                CurrentNotification = notification;
+                NotificationProgress = 100;
+
+                const int duration = 5000;
+                const int interval = 50;
+
+                for (var elapsed = 0; elapsed < duration; elapsed += interval)
+                {
+                    await Task.Delay(interval);
+
+                    NotificationProgress = 100.0 - ((double)elapsed / duration * 100.0);
+                }
+
+                if (NotificationsQueue.Count > 0 && NotificationsQueue[0] == notification)
+                {
+                    NotificationsQueue.RemoveAt(0);
+                }
+                
+                CurrentNotification = null;
+                NotificationProgress = 0;
+                IsNotificationToastVisible = false;
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+            }
+        }
+        finally
+        {
+            IsNotificationProcessorActive = false;
         }
     }
     
